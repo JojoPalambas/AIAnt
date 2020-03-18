a11cffff78d847ee246d1ed7769758ed49a20951
Gabriel Depotte <depott_g@epita.fr>
2020-03-18 19:50:41 +0100

    [motor] Make the map tiles instantiation

---
+++ Assets/Motor/Backstage/CoordConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CoordConverter
+{
+    // Converts hexagon axial coordinates to world position coordinates
+    // https://www.redblobgames.com/grids/hexagons/
+    // https://en.wikipedia.org/wiki/Hexagon
+    public static Vector2 HexToPos(Vector2Int hex)
+    {
+        // Coordinates given by the transformation of the x component of hex
+        Vector2 xTransformation = new Vector2((float)(hex.x) * Mathf.Sqrt(3) / 2, 0);
+        // Coordinates given by the transformation of the y component of hex
+        Vector2 yTransformation = new Vector2((float)(hex.y) * Mathf.Sqrt(3) / 4, (float)(hex.y) * .75f);
+
+        return xTransformation + yTransformation;
+    }
+}
--- Assets/Motor/Managers/GameManager.cs
+++ Assets/Motor/Managers/GameManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Prefabs")]
+    public GameObject groundTilePrefab;
+    public GameObject waterTilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = -9; i <= 9; i++)
+        {
+            for (int j = -9; j <= 9; j++)
+            {
+                Vector2 currentHexPosition = CoordConverter.HexToPos(new Vector2Int(i, j));
+                Instantiate(groundTilePrefab, new Vector3(currentHexPosition.x, groundTilePrefab.transform.position.y, currentHexPosition.y), groundTilePrefab.transform.rotation);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
