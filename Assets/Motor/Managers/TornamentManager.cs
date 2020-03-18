5ac8ff6006c7f6a96219e8376481f9bba3cc58bc
Gabriel Depotte <depott_g@epita.fr>
2020-03-18 12:39:57 +0100

    [motor] Setup the project tree

---
+++ Assets/Motor/Managers/TornamentManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornamentManager : MonoBehaviour
+{
+    [Header("General")]
+    public GameManager gameManagerPrefab;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = Instantiate(gameManagerPrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
