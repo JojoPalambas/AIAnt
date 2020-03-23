afc8ba64b1a22c55d488bbeaf32f153056a1ff03
Gabriel Depotte <depott_g@epita.fr>
2020-03-23 23:44:20 +0100

    [motor] Removed unused imports

--- Assets/Motor/DevUniverse/CommunicateReport.cs
+++ Assets/Motor/DevUniverse/CommunicateReport.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public class CommunicateReport
 {
     public readonly AntType type;
--- Assets/Motor/DevUniverse/PastTurnDigest.cs
+++ Assets/Motor/DevUniverse/PastTurnDigest.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 public class PastTurnDigest
 {
     public readonly Decision pastDecision;
